0ebf982f3299066dce822eab2b70f73f2bbe65ab
Valery Vishnyakov <balepc@gmail.com>
2012-04-20 19:41:50 +0300

    Update lib/resque/pool/pooled_worker.rb

--- lib/resque/pool/pooled_worker.rb
+++ lib/resque/pool/pooled_worker.rb
@@ -16,6 +16,36 @@ class Resque::Pool
     def shutdown?
       super || pool_master_has_gone_away?
     end
+    
+    def work(interval = 5.0, &block)
+      interval = Float(interval)
+      $0 = "resque: Starting"
+      startup
+
+      loop do
+        break if shutdown?
+
+        if not paused? and job = reserve
+          log "got: #{job.inspect}"
+          job.worker = self
+          run_hook :before_fork, job
+          working_on job
+
+            perform(job, &block)
+
+          done_working
+          @child = nil
+        else
+          break if interval.zero?
+          log! "Sleeping for #{interval} seconds"
+          procline paused? ? "Paused" : "Waiting for #{@queues.join(',')}"
+          sleep interval
+        end
+      end
+
+    ensure
+      unregister_worker
+    end
 
   end
 end
