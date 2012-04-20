760271f03f3041f036887bc872fc509e400660bd
Valery Vishnyakov <balepc@gmail.com>
2012-04-20 20:07:23 +0300

    hz

--- lib/resque/pool/pooled_worker.rb
+++ lib/resque/pool/pooled_worker.rb
@@ -1,7 +1,6 @@
-require 'resque/worker'
-
 class Resque::Pool
-  module PooledWorker
+
+  class PooledWorker < ::Resque::Worker
 
     def initialize(*args)
       @pool_master_pid = Process.pid
@@ -16,36 +15,7 @@ class Resque::Pool
     def shutdown?
       super || pool_master_has_gone_away?
     end
-    
-    def work(interval = 5.0, &block)
-      interval = Float(interval)
-      $0 = "resque: Starting"
-      startup
 
-      loop do
-        break if shutdown?
-
-        if not paused? and job = reserve
-          log "got: #{job.inspect}"
-          job.worker = self
-          run_hook :before_fork, job
-          working_on job
-
-            perform(job, &block)
-
-          done_working
-          @child = nil
-        else
-          break if interval.zero?
-          log! "Sleeping for #{interval} seconds"
-          procline paused? ? "Paused" : "Waiting for #{@queues.join(',')}"
-          sleep interval
-        end
-      end
-
-    ensure
-      unregister_worker
-    end
 
     # This is a hack, that makes resque-pool crazy!
     #
@@ -82,8 +52,5 @@ class Resque::Pool
 
 
   end
-end
 
-Resque::Worker.class_eval do
-  include Resque::Pool::PooledWorker
 end
